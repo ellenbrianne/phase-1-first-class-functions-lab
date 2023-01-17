176c21ed4c0f59ce2bbb48f5129e080a19b3d5bd
Ellen Bennett <bennette831@gmail.com>
2023-01-16 22:29:54 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers) {
+    let firstTwo = drivers.slice(0,2);
+    return firstTwo;
+};
+
+const returnLastTwoDrivers = function (drivers) {
+    let lastTwo = drivers.slice(2);
+    return lastTwo;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = num => {
+    return function (fare) {
+        return num * fare;
+    };
+};
+
+const fareDoubler = function (fare) {
+    return fare * 2;
+};
+
+const fareTripler = function (fare) {
+    return fare * 3;
+};
+
+const selectDifferentDrivers = function (drivers, driverFunction) {
+    if (driverFunction === returnFirstTwoDrivers) {
+        return drivers.slice(0,2);
+    } else {
+        return drivers.slice(2);
+    };
+};
